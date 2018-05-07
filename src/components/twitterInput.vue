1f0a9ac26dab6080cb7f2dd67ab0b84a2d5de916
UrosPurtic <uros.purtic@gmail.com>
2018-05-07 20:48:28 +0200

    inital commit

---
+++ src/components/twitterInput.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <textarea v-model="text" @input="handleChange"> </textarea>
+
+        <p>{{ numberOfCaracters }}/{{ maxNumberOfCaracters }}</p>
+    </div>
+</template>
+
+<script>
+
+    const MAX_NUMBER_OF_CARACTERS = 120
+
+    export default {
+        name: "TwitterInput",
+        data() {
+            return {
+
+                text: '',
+                maxNumberOfCaracters: MAX_NUMBER_OF_CARACTERS
+            }
+
+        },
+        computed: {
+            numberOfCaracters() {
+
+                return this.text.length
+            }
+
+        },
+        methods: {
+
+            handleChange(event) {
+
+
+                if (this.numberOfCaracters >= this.maxNumberOfCaracters) {
+
+
+
+                this.text = this.text.slice(0,this.maxNumberOfCaracters)
+
+
+                }
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
